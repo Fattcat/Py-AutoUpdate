9a222f7f2920678a2c054f63d99855cc7ded5352
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-11-29 15:47:30 +0100

    Update PY-AutoUpdate.py

--- PY-AutoUpdate.py
+++ PY-AutoUpdate.py
@@ -1,9 +1,11 @@
 import requests
 import git
 import os
+import shutil
+import subprocess
 
 # Nastav URL tvojho GitHub repozitára
-repo_url = 'https://github.com/fattcat/Py-AutoUpdate/AutoGit'
+repo_url = 'https://github.com/MyAcc/Python/AutoGit'
 
 # Funkcia na stiahnutie poslednej verzie z GitHub
 def download_latest_version():
@@ -21,18 +23,33 @@ def update_code():
     if os.path.exists(repo_dir):
         repo = git.Repo(repo_dir)
         origin = repo.remotes.origin
-        origin.pull()
+        current_commit = repo.head.object.hexsha
+
+        # Skontrolovať, či sa obsah zmenil
+        origin.fetch()
+        if current_commit != origin.refs.master.commit.hexsha:
+            print('Obsah sa zmenil. Aktualizácia...')
+            shutil.rmtree(repo_dir)  # Vymazanie starého priečinku
+            git.Repo.clone_from(repo_url, repo_dir)  # Stiahnutie nového obsahu
+            print('Obsah bol aktualizovaný.')
+        else:
+            print('Obsah sa nezmenil. Nie je potrebná aktualizácia.')
     else:
         git.Repo.clone_from(repo_url, repo_dir)
 
+        # Spustiť python skript, ak existuje
+        script_path = os.path.join(repo_dir, 'your_script.py')
+        if os.path.exists(script_path):
+            subprocess.run(['python', script_path])
+
 # Získať aktuálnu verziu kódu
-current_version = 'verzia-tvojho-aktualneho-kodu'
+current_version = '1.0.0'
 
 # Získať poslednú verziu z GitHub
 latest_version = download_latest_version()
 
 if latest_version and latest_version != current_version:
-    print('Nová verzia kódu je dostupná. Aktualizácia...')
+    print('Nová verzia kódu je dostupná...\nPrebieha Aktualizácia...')
     update_code()
     print('Kód bol aktualizovaný na najnovšiu verziu.')
 else:
